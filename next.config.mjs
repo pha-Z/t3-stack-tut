da4736bc2c8d3d49785a7dd64d378787edaacd64
6461 <pha-Z@users.noreply.github.com>
2023-10-16 01:53:27 +0200

    ignore build errors on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default config;
