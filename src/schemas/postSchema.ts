075bb805dcaa740228f358af64dcae791a794940
6461 <pha-Z@users.noreply.github.com>
2023-10-16 10:35:24 +0200

    improved error message

--- src/schemas/postSchema.ts
+++ src/schemas/postSchema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 export const postContentSchema = z.object({
   content: z
     .string()
-    .min(1)
-    .max(280)
+    .min(1, "too short!")
+    .max(280, "too long!")
     // check for `emoji` or `emoji ` https://regexper.com/#%2F%5E%28%5Cp%7BEmoji%7D%7C%28%5Cp%7BEmoji%7D%5Cs%29%29%2B%24%2Fu
     .regex(/^(\p{Emoji}|(\p{Emoji}\s))+$/u, {
       message: "use emojis only!",
