cb60c2cc81f36b8292fc5292bc94c7e47f0679e4
6461 <pha-Z@users.noreply.github.com>
2023-10-16 03:32:14 +0200

    added comments explaining shape validation

--- src/schemas/postSchema.ts
+++ src/schemas/postSchema.ts
@@ -4,14 +4,17 @@ export const postContentSchema = z.object({
     .string()
     .min(1)
     .max(280)
+    // check for `emoji` or `emoji ` (with whitespace)
     .regex(/^(\p{Emoji}|(\p{Emoji}\s))+$/u, {
       message: "use emojis only!",
     })
+    // check that every array element (split at whitespace) is not a number (NaN)
+    // required because numbers bypass emoji regex 🥴
     .refine(
       (content: string) =>
         content
           .split(" ")
-          .map((x) => parseInt(x))
+          .map((el) => parseInt(el))
           .every(isNaN),
       { message: "use emojis only!" },
     ),
